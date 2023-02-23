81e5ea420e30050468ecc29e2717e381e4586f51
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-02-24 01:01:44 +0530

    Wallet Connection is Done

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CoinList from '../components/CoinList'
+
+const HomePage = () => {
+  return (
+    <>
+        <CoinList/>
+    </>
+  )
+}
+
+export default HomePage
